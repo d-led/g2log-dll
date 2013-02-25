a68b37b28defd6693d520c65b67af4981688b541
d-led <dmlled@yahoo.com>
2013-02-26 00:22:35 +0100

    no crash, no deadlock

--- g2log_dll.cpp
+++ g2log_dll.cpp
@@ -6,19 +6,15 @@
 #include <memory>
 #include <iostream>
 
-static std::unique_ptr<g2LogWorker> current_worker=nullptr;
-
 extern "C" int init_g2log (const char* prefix,const char* location) {
 
-	current_worker.reset(new g2LogWorker(prefix, location));
-	g2::initializeLogging(current_worker.get());
+	g2::initializeLogging(new g2LogWorker(prefix, location));
 
-	return current_worker.get()!=nullptr?1:0;
+	return 1;
 }
 
 extern "C" void stop_g2log () {
 	g2::shutDownLogging();
-	current_worker=0;
 }
 
 extern "C" void printf_g2log(const char* _file,const int _line,const char* _function,const char* _level,const char* _message)
